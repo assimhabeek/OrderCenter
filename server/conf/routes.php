ce156c50d99454bcb520906f4c8a4f5574c99e78
assim <assim@seriescode.com>
2021-02-24 11:58:09 +0100

    Replace delete with post

--- server/conf/routes.php
+++ server/conf/routes.php
@@ -106,7 +106,7 @@ return function (App $app) {
             
             $adminGroup->post('/shopify-webhooks', ShopifyConnector::class . ':createWebhooks');
             $adminGroup->get('/shopify-webhooks', ShopifyConnector::class . ':getWebhooks');
-            $adminGroup->delete('/shopify-webhooks', ShopifyConnector::class . ':deleteWebhooks');
+            $adminGroup->post('/shopify-webhooks/remove', ShopifyConnector::class . ':deleteWebhooks');
 
 
         })->add(AdminMiddleware::class);
